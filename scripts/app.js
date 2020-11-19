5af73f1df946336c03172a0e79e7dfb8c352d967
Penelope Jungreis <penelope.jungreis@gmail.com>
2020-11-19 17:57:45 +0000

    basic html and css for grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,62 @@
 function init() {
-  window.alert("JS is working!")
+  const grid = document.querySelector('.grid')
+  const cells = []
+
+  const width = 20
+  const cellCount = width * width
+
+  // make div in css
+
+
+  // make grid using JS
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement('div')
+    cell.innerHTML = i
+    grid.appendChild(cell)
+    cells.push(cell)
+  }
+
+  // lightly style board
+  // make obvious what everything is with classes and semantic html tags
+  // add wall and fish styling as borders and background images
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ! DO NOT EDIT below this line !
 }
 
 window.addEventListener('DOMContentLoaded', init)
