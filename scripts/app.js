db0c7a2a01029b0e2a8608adf85068c8e9894cc4
Penelope Jungreis <penelope.jungreis@gmail.com>
2020-11-20 13:37:56 +0000

    orcaClass, add orca function, orca on random cell

--- scripts/app.js
+++ scripts/app.js
@@ -7,8 +7,10 @@ function init() {
   const fishClass = 'fish'
   const wallClass = 'wall'
   const sharkClass = 'shark'
+  const orcaClass = 'orca'
 
   let sharkPosition = 365
+  let orcaPosition = 25
 
   // make div in css
 
@@ -25,40 +27,38 @@ function init() {
   // make obvious what everything is with classes and semantic html tags
 
   // add wall and fish styling as borders and background images
-
+  // ! VERY LONG WALL BUILDER LOOP
   cells.forEach(cell => {
     const coral = parseInt(cell.dataset.id)
     // horizontal walls
     if (coral >= 0 && coral <= 19
     || coral >= 28 && coral <= 31
-    || coral >= 48 && coral <= 51
-    || coral >= 68 && coral <= 71
     || coral >= 42 && coral <= 46
-    || coral >= 62 && coral <= 66
+    || coral >= 48 && coral <= 51
     || coral >= 53 && coral <= 57
+    || coral >= 62 && coral <= 66
+    || coral >= 68 && coral <= 71
     || coral >= 73 && coral <= 77
     || coral >= 102 && coral <= 117
     || coral >= 141 && coral <= 145
-    || coral >= 161 && coral <= 165
-    || coral >= 181 && coral <= 185
-    || coral >= 201 && coral <= 205
-    || coral >= 207 && coral <= 212
     || coral >= 154 && coral <= 158
+    || coral >= 161 && coral <= 165
     || coral >= 174 && coral <= 178
-    || coral >= 194 && coral <= 198
+    || coral >= 181 && coral <= 185
+    || coral >= 207 && coral <= 212
     || coral >= 214 && coral <= 218
     || coral >= 221 && coral <= 225
-    || coral >= 234 && coral <= 238
     || coral >= 227 && coral <= 232
+    || coral >= 234 && coral <= 238
     || coral >= 249 && coral <= 250
-    || coral >= 269 && coral <= 270
     || coral >= 262 && coral <= 265
+    || coral >= 269 && coral <= 270
     || coral >= 274 && coral <= 277
-    || coral >= 294 && coral <= 297
-    || coral >= 314 && coral <= 317
     || coral >= 282 && coral <= 285
+    || coral >= 294 && coral <= 297
     || coral >= 302 && coral <= 305
     || coral >= 307 && coral <= 312
+    || coral >= 314 && coral <= 317
     || coral >= 327 && coral <= 332
     || coral >= 342 && coral <= 357
     || coral >= 380 && coral <= 399
@@ -72,7 +72,6 @@ function init() {
     || coral === 140
     || coral === 160
     || coral === 180
-    || coral === 200
     || coral === 210
     || coral === 220
     || coral === 240
@@ -91,7 +90,6 @@ function init() {
     || coral === 120 + 19
     || coral === 140 + 19
     || coral === 160 + 19
-    || coral === 180 + 19
     || coral === 200 + 19
     || coral === 220 + 19
     || coral === 240 + 19
@@ -124,10 +122,42 @@ function init() {
     }
   })
 
+
+  // ? GAME PLAY FUNCTIONS *********
   function checkIfWall(position) {
     return cells[position].classList.contains(wallClass)
   }
 
+  function addShark(position) {
+    cells[position].classList.add(sharkClass)
+  }
+
+  function removeShark(position) {
+    cells[position].classList.remove(sharkClass)
+  }
+
+  function addOrca(position) {
+    cells[position].classList.add(orcaClass)
+  }
+
+  function removeOrca(position) {
+    cells[position].classList.remove(orcaClass)
+  }
+
+  function removeFish(position) {
+    cells[position].classList.remove(fishClass)
+  }
+
+
+  // MAKE GHOST KILLER WHALES:
+  // - give killer whales the ability to move to new tiles and disappear from old ones
+  // - set random number
+  // - set random movement for whales with numbers
+  // * MOVE ORCA FUNCTION **********
+  const randomPosition = Math.floor(Math.random() * 100)
+  if (!checkIfWall(randomPosition)) orcaPosition = randomPosition
+
+  // * MOVE PAC-SHARK FUNCTION **********
   function handleKeyUp(event) {
     removeShark(sharkPosition)
     removeFish(sharkPosition)
@@ -159,19 +189,14 @@ function init() {
     addShark(sharkPosition)
   }
 
-  function addShark(position) {
-    cells[position].classList.add(sharkClass)
-  }
+  addOrca(orcaPosition)
 
-  function removeShark(position) {
-    cells[position].classList.remove(sharkClass)
-  }
 
-  function removeFish(position) {
-    cells[position].classList.remove(fishClass)
-  }
+
 
   document.addEventListener('keydown', handleKeyUp)
+  
+  
   // - create variable for tracking sharky's place on the grid
   // - add an event listener to move with arrow keys around the board ('keydown')
   // - update sharky's place and remove sharky from previous place with each move
